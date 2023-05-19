37844527026a04d7d54f6fe2060928775bb54100
Madi <abaymadi1@gmail.com>
2023-05-19 15:39:41 +0600

    Realize WebSocket in FastAPI

--- code/main.py
+++ code/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request, WebSocket
+from fastapi.responses import JSONResponse, HTMLResponse
+
 from code import database
 from code.users import routers as user_routers
-
 
 app = FastAPI()
 app.include_router(user_routers.router)
@@ -28,3 +28,51 @@ async def hello(request: Request):
     return {
         'request_url': request.query_params
     }
+
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:9000/websocket");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.get("/chat/")
+async def chat():
+    return HTMLResponse(html)
+
+
+@app.websocket("/websocket")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
